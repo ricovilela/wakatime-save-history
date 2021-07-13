62671414f97399d13181f9c3d620c1fe6230abcd
Rico Vilela <rico.vilela@gmail.com>
2021-07-13 10:18:31 -0300

    Delete dist directory

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const TimelineController_1 = require("../controllers/TimelineController");
-const routes = express_1.Router();
-exports.routes = routes;
-const timelineController = new TimelineController_1.TimelineController();
-routes.get('/:name?', timelineController.timeLine);
