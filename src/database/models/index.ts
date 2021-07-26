e65f57e59329bc8ead1463921e273bddc9492255
ricovilela <rico.vilela@gmail.com>
2021-07-26 11:58:56 -0300

    fix: on database model, implementated to read both files, .ts and .js

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -19,7 +19,9 @@ const sequelize = new Sequelize(
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      (file.slice(-3) === ".ts" || file.slice(-3) === ".js")
     );
   })
   .forEach((file) => {
